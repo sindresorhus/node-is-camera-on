dbeaba90ad0d1210678eb55caf7d086eeeb53986
Sindre Sorhus <sindresorhus@gmail.com>
2017-06-10 01:50:08 +0200

    Minor tweaks

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 'use strict';
+const path = require('path');
 const macosVersion = require('macos-version');
 const execa = require('execa');
 
+const bin = path.join(__dirname, '.build/release/main');
+
 module.exports = () => Promise.resolve().then(() => {
 	macosVersion.assertGreaterThanOrEqualTo('10.11');
-	return execa.stdout('./.build/release/main', {cwd: __dirname}).then(x => x === 'true');
+	return execa.stdout(bin).then(x => x === 'true');
 });
